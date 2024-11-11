9f9612bf0ced0b192d8b407b2a3ff42a43f8cc6a
Bavin <bavinachu465@gmail.com>
2024-11-11 18:33:22 +0530

    add folder

---
+++ pattern/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// pattern printing in c
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 3; i++)
+    {
+        for (j = 1; j <= 6; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
